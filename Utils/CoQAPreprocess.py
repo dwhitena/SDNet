7e0da1cbe1daffd4e82c94fd150c215b9c0d83c6
Daniel Whitenack <whitenack.daniel@gmail.com>
2019-05-02 14:28:26 -0400

    update conf file path

--- Utils/CoQAPreprocess.py
+++ Utils/CoQAPreprocess.py
@@ -23,7 +23,7 @@ class CoQAPreprocess():
         self.spacyDir = opt['FEATURE_FOLDER']
         self.train_file = os.path.join(opt['datadir'], opt['CoQA_TRAIN_FILE'])
         self.dev_file = os.path.join(opt['datadir'], opt['CoQA_DEV_FILE'])
-        self.glove_file = os.path.join(opt['datadir'], opt['INIT_WORD_EMBEDDING_FILE'])
+        self.glove_file = opt['INIT_WORD_EMBEDDING_FILE']
         self.glove_dim = 300
         self.official = 'OFFICIAL' in opt
         self.data_prefix = 'coqa-'
